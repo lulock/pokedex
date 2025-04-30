b5cba7e31129f16f17cb3acc32140c841c1c63b9
lulock <leilamethnani@gmail.com>
2025-04-30 09:15:33 +0200

    update main.go to get data from PokeAPI

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 	"bufio"
 	"os"
+	"net/http"
+	"encoding/json"
+	"io"
 )
 
 // this is a registry of commands
@@ -12,7 +15,22 @@ import (
 type cliCommand struct {
 	name string
 	description string
-	callback func() error
+	callback func(*config) error
+}
+
+type config struct {
+	Next string
+	Previous string
+}
+
+type LocationAreas struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
 }
 
 
@@ -25,26 +43,99 @@ func cleanInput(text string) []string {
 }
 
 // exits the programme
-func commandExit() error {
+func commandExit(conf *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	return nil
+}
+
+// displays the names of 20 location areas in the Pokemon world
+
+func commandMap(conf *config) error {
+	resp, err := http.Get(conf.Next)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Could not get locations:%v",err))
+		return err
+	}
+
+	locAreas := LocationAreas{}
+
+	data, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	err = json.Unmarshal(data, &locAreas)
+	
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	
+	for _, loc := range locAreas.Results {
+		fmt.Println(loc.Name)
+	}
+	conf.Next = locAreas.Next
+	conf.Previous = locAreas.Previous
+	return nil
+}
+
+func commandMapb(conf *config) error {
+	resp, err := http.Get(conf.Previous)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Could not get locations:%v",err))
+		return err
+	}
+
+	locAreas := LocationAreas{}
+
+	data, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	err = json.Unmarshal(data, &locAreas)
+	
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	
+	for _, loc := range locAreas.Results {
+		fmt.Println(loc.Name)
+	}
+	conf.Next = locAreas.Next
+	conf.Previous = locAreas.Previous
 	return nil
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin) // wait for user input using bufio.NewScanner which blocks the code and waits for input, once the user types something and presses enter, the code continues and the input is available in the returned bufio.Scanner
 	// map the supported commands:
+	conf := config{Next: "https://pokeapi.co/api/v2/location-area/"}
 	validCommands := map[string]cliCommand{
 		"exit": {
 			name: "exit",
 			description: "Exit the Pokedex",
 			callback: commandExit,
 		},
+		"map": {
+			name: "map",
+			description: "Displays the names of the next 20 location areas in the Pokemon world",
+			callback: commandMap,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Displayes the names of the previous 20 locations in the Pokemon world",
+			callback: commandMapb,
+		},
 	}
 	validCommands["help"] = cliCommand{
 			name: "help",
 			description: "Displays a help message",
-			callback: func() error {
+			callback: func(conf *config) error {
 				fmt.Println("Welcome to the Pokedex!")
 				fmt.Println("Usage:")
 				fmt.Println()
@@ -65,7 +156,7 @@ func main() {
 				if !ok {
 					fmt.Println("Unknown command")
 				} else {
-					if err := cmd.callback(); err != nil {
+					if err := cmd.callback(&conf); err != nil {
 					fmt.Println(err)
 					}
 				}
